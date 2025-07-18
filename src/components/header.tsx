88dd688a8e6bc2e58395bedeb1f02f488641de78
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-07-17 23:06:22 -0600

    Add header layout with navigation and search bar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,16 @@
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="fixed grid grid-cols-3 items-center h-[60px] bg-dark-secondary w-full overflow-hidden px-main-x">
+      <a href="/" className="hover:brightness-125 transition">
+        <img src="/vite.svg" alt="Logo" className="h-8 w-auto" />
+      </a>
+      <input className="max-h-[calc(100%-25px)] size-full px-2.5 bg-zinc-800 rounded-lg outline-none font-medium"></input>
+      <a
+        href="/user"
+        className="flex justify-end hover:brightness-125 transition"
+      >
+        <img src="/vite.svg" alt="Logo" className="h-8 w-auto" />
+      </a>
+    </header>
+  );
 }
