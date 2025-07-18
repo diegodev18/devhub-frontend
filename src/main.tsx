675281a1c608f42c9bd1281ed8f9833740030658
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-07-17 22:55:12 -0600

    Add basic header and footer components

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "/src/styles/global.css";
 import App from "./App.tsx";
+import Footer from "./components/footer.tsx";
+import Header from "./components/header.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Header />
+    <main>
+      <App />
+    </main>
+    <Footer />
   </StrictMode>,
 );
