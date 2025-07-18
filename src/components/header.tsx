3aba777a9e51c70cd9a050cbf87cbd367cb25bef
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-07-17 23:12:11 -0600

    Improve header layout and hover animations

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,17 @@
 export default function Header() {
   return (
-    <header className="fixed grid grid-cols-3 items-center h-[60px] bg-dark-secondary w-full overflow-hidden px-main-x">
-      <a href="/" className="hover:brightness-125 transition">
-        <img src="/vite.svg" alt="Logo" className="h-8 w-auto" />
-      </a>
-      <input className="max-h-[calc(100%-25px)] size-full px-2.5 bg-zinc-800 rounded-lg outline-none font-medium"></input>
-      <a
-        href="/user"
-        className="flex justify-end hover:brightness-125 transition"
-      >
-        <img src="/vite.svg" alt="Logo" className="h-8 w-auto" />
-      </a>
+    <header className="fixed grid grid-cols-[auto_1fr_auto] gap-x-main-x items-center h-[60px] bg-dark-secondary w-full overflow-hidden px-main-x">
+      <div className="hover:brightness-125 transition">
+        <a href="/" className="w-fit hover:scale-105 transition">
+          <img src="/vite.svg" alt="Logo" className="h-8 w-auto" />
+        </a>
+      </div>
+      <input className="max-h-[calc(100%-25px)] max-w-[600px] mx-auto size-full px-2.5 bg-zinc-800 rounded-lg outline-none font-medium"></input>
+      <div className="flex justify-end hover:brightness-125 transition">
+        <a href="/user" className="w-fit hover:scale-105 transition">
+          <img src="/vite.svg" alt="Logo" className="h-8 w-auto" />
+        </a>
+      </div>
     </header>
   );
 }
